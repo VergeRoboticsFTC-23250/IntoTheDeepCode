2d8f89f676317e877d01803090df7646db1edd19
Rohan Gopalan <130528515+ElectroRG@users.noreply.github.com>
2024-12-06 23:36:47 -0600

    1+3 bucket pathing

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/Auto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/Auto.java
@@ -14,9 +14,26 @@ public class Auto extends LinearOpMode {
 
     @Override
     public void runOpMode() throws InterruptedException {
+        chassis = new Chassis(hardwareMap);
+
         waitForStart();
+
         Actions.runBlocking(chassis.md.actionBuilder(chassis.md.pose)
-                .strafeTo(new Vector2d(-10, 33))
+                .strafeTo(new Vector2d(10, 33))
+                .waitSeconds(.7)
+                .lineToY(40)
+                .strafeToLinearHeading(new Vector2d(54,47),Math.toRadians(250))
+                .waitSeconds(1)
+                .strafeToLinearHeading(new Vector2d(60,58),Math.toRadians(240))
+                .waitSeconds(.7)
+                .strafeToLinearHeading(new Vector2d(58,47),Math.toRadians(270))
+                .waitSeconds(1)
+                .strafeToLinearHeading(new Vector2d(60,58),Math.toRadians(250))
+                .waitSeconds(.7)
+                .strafeToLinearHeading(new Vector2d(53,44),Math.toRadians(315))
+                .waitSeconds(1)
+                .strafeToLinearHeading(new Vector2d(62,57),Math.toRadians(250))
+                .waitSeconds(.7)
                 .build());
         // Add your code here
     }
